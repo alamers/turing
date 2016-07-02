fe4442e4c43291a4b724917e3698ccfc73c253fd
Arjan Lamers <arjanl@aardbeitje.nl>
2016-07-02 22:10:09 +0200

    started working on flipping bits

--- src/nl/aardbeitje/turing/Test.java
+++ src/nl/aardbeitje/turing/Test.java
@@ -21,9 +21,9 @@ public class Test {
 
 	private static final int WRITE_ANGLE = 180;
 	private static final int READ_ANGLE = 49;
-	private static final int READER_ARM_SPEED=80;
+	private static final int READER_ARM_SPEED = 80;
 	private static final int MEAN_SIZE = 30;
-
+	private static final int DEGREES_PER_BIT = 1800;
 
 	private final RegulatedMotor writer;
 	private final RegulatedMotor readerArm;
@@ -34,11 +34,9 @@ public class Test {
 		LCD.drawString("Turing Machine", 0, 0);
 
 		Test test = new Test();
-		
-		test.testTape();
-		
-//		test.calibrate();
-//		test.testCalibrate();
+
+		 test.calibrate();
+		 test.writeOnesAndZeros();
 	}
 
 	public Test() {
@@ -47,7 +45,7 @@ public class Test {
 		tape = new EV3MediumRegulatedMotor(TAPE_PORT);
 
 		readerArm.setSpeed(READER_ARM_SPEED);
-		
+
 		EV3ColorSensor sensor = new EV3ColorSensor(READER_PORT);
 		sensor.setCurrentMode("Red");
 		MeanFilter mean = new MeanFilter(sensor, MEAN_SIZE);
@@ -56,49 +54,72 @@ public class Test {
 		reader.setScaleCalibration(-1, 1);
 	}
 
+	private void writeOnesAndZeros() {
+		final int steps = 10;
+		for (int i=0; i<steps; i++) {
+			boolean current = read();
+			boolean future = i%2==1;
+			
+			if (current!=future) {
+				if (future) {
+					write0();
+				} else {
+					write1();
+				}
+			}
+			
+			forward();
+		}
+		
+		for (int i=0; i<steps; i++) {
+			backward();
+		}
+	}
+	
 	private void calibrate() {
 		final int samplesPerWrite = 2;
 		final int writes = 3;
-		
+
 		reader.startCalibration();
 		reader.suspendCalibration();
-		
+
 		float[] tmp = new float[reader.sampleSize()];
-		
-		for( int j=0; j<writes; j++ ) {
-			for( int i=0; i<samplesPerWrite; i++) {
+
+		for (int j = 0; j < writes; j++) {
+			for (int i = 0; i < samplesPerWrite; i++) {
 				readerArm.rotate(READ_ANGLE);
-				
+
 				reader.resumeCalibration();
-				for( int k=0;k<MEAN_SIZE; k++) {
+				for (int k = 0; k < MEAN_SIZE; k++) {
 					reader.fetchSample(tmp, 0);
 				}
 				reader.suspendCalibration();
-				for (int k=0; k<reader.getScaleCorrection().length; k++) {
-					LCD.drawString( String.format("c: %2.3f / %2.3f", reader.getScaleCorrection()[k] , reader.getOffsetCorrection()[k]), 0, k+1);
+				for (int k = 0; k < reader.getScaleCorrection().length; k++) {
+					LCD.drawString(String.format("c: %2.3f / %2.3f", reader.getScaleCorrection()[k],
+							reader.getOffsetCorrection()[k]), 0, k + 1);
 				}
 				readerArm.rotate(-READ_ANGLE);
 			}
 			write1();
-			
-			for( int i=0; i<samplesPerWrite; i++) {
+
+			for (int i = 0; i < samplesPerWrite; i++) {
 				readerArm.rotate(READ_ANGLE);
 				reader.resumeCalibration();
-				for( int k=0;k<MEAN_SIZE; k++) {
+				for (int k = 0; k < MEAN_SIZE; k++) {
 					reader.fetchSample(tmp, 0);
 				}
 				reader.suspendCalibration();
 				readerArm.rotate(-READ_ANGLE);
 			}
-			
+
 			write0();
 		}
-		
-		reader.stopCalibration();		
+
+		reader.stopCalibration();
 	}
-	
+
 	private void testCalibrate() {
-		for (int i=0; i<5; i++) {
+		for (int i = 0; i < 5; i++) {
 			read();
 			Button.ENTER.waitForPressAndRelease();
 			write1();
@@ -107,38 +128,71 @@ public class Test {
 			write0();
 		}
 	}
-	
+
 	/**
-	 * Reads the current bit on the tape.
-	 * Moves the reader arm in, reads MEAN_SIZE samples and feeds it through the linear calibration filter to normalize to (-1,1).
+	 * Reads the current bit on the tape. Moves the reader arm in, reads
+	 * MEAN_SIZE samples and feeds it through the linear calibration filter to
+	 * normalize to (-1,1).
+	 * 
 	 * @return true for a 1, false for a 0
 	 */
 	private boolean read() {
 		readerArm.rotate(READ_ANGLE);
 		float[] tmp = new float[reader.sampleSize()];
-		for( int k=0;k<MEAN_SIZE; k++) {
+		for (int k = 0; k < MEAN_SIZE; k++) {
 			reader.fetchSample(tmp, 0);
 		}
-		LCD.drawString( String.format("read: %2.1f = %b" , tmp[0] , tmp[0]<0), 0, 3);
+		LCD.drawString(String.format("read: %2.1f = %b", tmp[0], tmp[0] < 0), 0, 3);
 		readerArm.rotate(-READ_ANGLE);
-		return tmp[0]<0;
+		return tmp[0] < 0;
 	}
 
+	/** 
+	 * Writes a 1. Make sure there is a 0 currently written or else we'll have a mechanical failure.
+	 */
 	private void write1() {
 		writer.rotate(WRITE_ANGLE);
 	}
 
+	/** 
+	 * Writes a 0. Make sure there is a 1 currently written or else we'll have a mechanical failure.
+	 */
 	private void write0() {
 		writer.rotate(-WRITE_ANGLE);
 	}
 
+	/**
+	 * Move the tape 1 position back to the beginning.
+	 */
+	private void backward() {
+		tape.rotate(-DEGREES_PER_BIT);
+	}
+	
+	/**
+	 * Move the tape 1 position further up the tape.
+	 */
+	private void forward() {
+		tape.rotate(DEGREES_PER_BIT);
+	}
+	
 	private void testTape() {
-		while( Button.ESCAPE.isUp()) {
-			if (Button.DOWN.isDown()) {
-				tape.rotate(20 * 360);
-			}
+		int rotations = 1800;
+		LCD.drawString(String.format("rot per : %d", rotations), 0, 3);
+
+		while (Button.ESCAPE.isUp()) {
 			if (Button.UP.isDown()) {
-				tape.rotate(-20 * 360);
+				tape.rotate(rotations);
+			}
+			if (Button.DOWN.isDown()) {
+				tape.rotate(-rotations);
+			}
+			if (Button.LEFT.isDown()) {
+				rotations -= 30;
+				LCD.drawString(String.format("rotations: %d", rotations), 0, 3);
+			}
+			if (Button.RIGHT.isDown()) {
+				rotations += 30;
+				LCD.drawString(String.format("rotations: %d", rotations), 0, 3);
 			}
 		}
 	}
